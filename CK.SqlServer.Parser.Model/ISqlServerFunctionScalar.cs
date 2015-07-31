90c5b98a78bfecc1aea26ba08406e65dced14848
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-07-31 17:11:34 +0200

    Added NuBuild project.

---
+++ CK.SqlServer.Parser.Model/ISqlServerFunctionScalar.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK.SqlServer.Parser
+{
+    public interface ISqlServerFunctionScalar : ISqlServerCallableObject
+    {
+        ISqlServerUnifiedTypeDecl ReturnType { get; }
+    }
+}
