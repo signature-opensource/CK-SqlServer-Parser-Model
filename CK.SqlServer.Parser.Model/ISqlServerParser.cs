fed0a1b667668d708f45a30d9ca096b376dfe508
Olivier Spinelli <olivier.spinelli@invenietis.com>
2015-08-05 12:25:52 +0200

    Extended ISqlServerParser.

--- CK.SqlServer.Parser.Model/ISqlServerParser.cs
+++ CK.SqlServer.Parser.Model/ISqlServerParser.cs
@@ -11,5 +11,11 @@ namespace CK.SqlServer.Parser
         ISqlServerParserError ParseObject( string text, out ISqlServerObject sqlObject );
 
         ISqlServerParserError ParseStoredProcedure( string text, out ISqlServerStoredProcedure sqlProcedure );
+
+        ISqlServerParserError ParseStoredFunctionScalar(string text, out ISqlServerFunctionScalar sqlFScalar);
+
+        ISqlServerParserError ParseStoredFunctionTable( string text, out ISqlServerFunctionTable sqlFTable );
+
+        ISqlServerParserError ParseStoredFunctionInlineTable( string text, out ISqlServerFunctionInlineTable sqlFInlineTable );
     }
 }
