95c2cc6982690817008ee9f0923f60ec55bc368b
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-05-13 23:24:04 +0200

    Parser support all kind of objects. Its interface avoids the out parameter.

--- CK.SqlServer.Parser.Model/ISqlServerParserError.cs
+++ CK.SqlServer.Parser.Model/ISqlServerParserError.cs
@@ -6,8 +6,13 @@ namespace CK.SqlServer.Parser
     /// <summary>
     /// Captures parsing error.
     /// </summary>
-    public interface ISqlServerParserError
+    public interface ISqlServerParserResult<T> where T : class, ISqlServerParsedText
     {
+        /// <summary>
+        /// Gets the result. Null on error.
+        /// </summary>
+        T Result { get; }
+
         /// <summary>
         /// Gets the error message.
         /// Null if <see cref="IsError"/> is false.
