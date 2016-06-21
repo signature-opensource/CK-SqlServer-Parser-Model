8c8417b9adcb310166c645042c7f07b7cd7f405a
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-06-21 18:31:03 +0200

    Added Transform method to ISqlServerTransformer.

--- CK.SqlServer.Parser.Model/ISqlServerTransformer.cs
+++ CK.SqlServer.Parser.Model/ISqlServerTransformer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CK.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,16 @@ namespace CK.SqlServer.Parser
         /// </summary>
         string TargetSchemaName { get; }
 
+        /// <summary>
+        /// Applies this transformation to an object.
+        /// This uses untyped object: there is no guaranty that a transformation
+        /// does not change the type of the transformed object.
+        /// Use <see cref="SqlServerExtension.SafeTransform{T}(ISqlServerTransformer, IActivityMonitor, T)">SafeTransform</see> 
+        /// extension method to ensure type preservation.
+        /// </summary>
+        /// <param name="monitor">The monitor that will receive logs and errors. Can not be null.</param>
+        /// <param name="target">The target object to transform. Can not be null.</param>
+        /// <returns>The transformed object on success and null if an error occurred.</returns>
+        object Transform( IActivityMonitor monitor, object target );
     }
 }
